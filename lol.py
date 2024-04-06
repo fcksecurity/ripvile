cd711ba27198f810a22433cbf2ba1894880c25a8
leon <164789111+fcksecurity@users.noreply.github.com>
2024-04-06 17:22:29 +0000

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,65 @@
+import time
+import threading
+import random
+import colorama
+import os
+import json
+import httpx
+import datetime
+from colorama           import Fore
+
+config = json.load(open("config.json", "r"))
+proxies = open("proxies.txt", "r").read().splitlines()
+banner = Fore.LIGHTBLUE_EX + '''
+       _          _ __   
+  ____(_)__ _  __(_) /__ 
+ / __/ / _ \ |/ / / / -_)       <3
+/_/ /_/ .__/___/_/_/\__/ 
+     /_/                 
+                                                                      
+'''
+
+class Logging():
+    def send(text, token:str):
+        timeez = datetime.datetime.now().strftime('%H:%M:%S')
+        token = token.split('.')[0]
+        print(f'{Fore.LIGHTBLACK_EX}{timeez} {Fore.LIGHTBLUE_EX}{text} {Fore.LIGHTBLACK_EX}| {Fore.LIGHTBLUE_EX}[{Fore.WHITE}{token}***{Fore.LIGHTBLUE_EX}]')
+
+    def send2(text):
+        timeez = datetime.datetime.now().strftime('%H:%M:%S')
+        print(f'{Fore.LIGHTBLACK_EX}{timeez} {Fore.LIGHTBLUE_EX}{text} {Fore.LIGHTBLACK_EX}')
+
+def send_request(channelid, token):
+    try:
+        session = httpx.Client(proxies = {"http://": random.choice(proxies)})
+        session.headers = {"Authorization": str(token)}
+
+        r = session.post(
+            url = f'https://discord.com/api/v9/channels/{channelid}/messages',
+            json = {
+                "content": config["message"]
+            }
+        )
+        if r.status_code == 429:
+            data = r.json()
+            retry = int(data["retry_after"])
+            Logging.send(f'Ratelimited: {retry}s', token)
+            time.sleep(retry)
+        else:
+            Logging.send('Successfully sent message', token)
+            time.sleep(float(config["cool_per_send"]))
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    os.system('cls')
+    os.system('title RIPVile @encq')
+    print(banner)
+    Logging.send2('Starting threads...')
+    cool = config["cool_per_thread"]
+    while True:
+        with open("tokens.txt", "r") as file:
+            tokens = file.read().splitlines()
+            for token in tokens:
+                threading.Thread(target=send_request, args=('1225510131626803312', token,)).start()
+                time.sleep(float(cool))
